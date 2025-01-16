00ae092f21e3b2d3249bcb0e1143a62bf946bb80
Salameh <hamed.salameh@philips.com>
2025-01-16 17:40:06 +0200

    wip

--- TelemetryService/Program.cs
+++ TelemetryService/Program.cs
@@ -4,9 +4,25 @@ using TelemetryService;
 var builder = Host.CreateApplicationBuilder(args);
 
 // Add MassTransit services
+builder.Services.AddOptions<MassTransitHostOptions>()
+    .Configure<IServiceProvider>((options, sp) =>
+    {
+        options.WaitUntilStarted = false;
+        options.StartTimeout = TimeSpan.FromSeconds(10);
+        options.StopTimeout = TimeSpan.FromSeconds(10);
+        options.ConsumerStopTimeout = TimeSpan.FromSeconds(10);
+
+
+        // Filters
+        
+    });
+
 builder.Services.AddMassTransit(x =>
 {
-    x.AddConsumer<TelemetryDataConsumer>();
+    x.AddConsumer<TelemetryDataConsumer>(configure =>
+    {
+        configure.UseMessageRetry(r => r.Immediate(5));
+    });
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.Host("localhost", "/", host =>
@@ -16,13 +32,28 @@ builder.Services.AddMassTransit(x =>
         });
 
         cfg.ConfigureEndpoints(context);
+
+        // filters
+        cfg.UseKillSwitch(options =>
+        {
+            options.SetActivationThreshold(10);
+            options.SetTripThreshold(0.15);
+            options.SetRestartTimeout(TimeSpan.FromSeconds(30));
+        });
+
+        cfg.UseCircuitBreaker(cb =>
+        {
+            cb.TrackingPeriod = TimeSpan.FromMinutes(1);
+            cb.TripThreshold = 20;
+            cb.ActiveThreshold = 10;
+            cb.ResetInterval = TimeSpan.FromMinutes(5);
+        });
     });
 });
 
 builder.Services.AddLogging();
 builder.Services.AddSingleton<ITelemetryDataProcessor, TelemetryDataProcessor>();
 builder.Services.AddSingleton<IAnomalyDetector, AnomalyDetector>();
-builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
 host.Run();
